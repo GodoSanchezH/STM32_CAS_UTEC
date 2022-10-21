42a70f86b2919352502770372149b4750c923752
Godo Sanchez Heredia <gssanchezh@ieee.org>
2022-10-21 18:46:41 -0500

    v3

--- Ejemplo1_GPIO_P/Core/Src/main.c
+++ Ejemplo1_GPIO_P/Core/Src/main.c
@@ -96,17 +96,22 @@ int main(void)
   while (1)
   {
 
-	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	/* HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 	 HAL_Delay(100);
 	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	 HAL_Delay(100);
+	 HAL_Delay(100);*/
+
+			if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)) {
+
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+			}else{
+
+				 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+			}
+
 
     /* USER CODE END WHILE */
-
-
-
-
-
 
     /* USER CODE BEGIN 3 */
   }
@@ -173,10 +178,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
